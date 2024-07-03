a772b1d57daae9fab12127ff8f254e1cc1a7c5b1
aadityasingh2233 <133959646+aadityasingh2233@users.noreply.github.com>
2024-07-03 20:47:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+var randomNumber1=Math.round(Math.random()*6);
+
+var randomimage="image/dice" + randomNumber1 + ".png";
+var image1source1=document.querySelector("img")[0].setAttribute("src",randomimage);
